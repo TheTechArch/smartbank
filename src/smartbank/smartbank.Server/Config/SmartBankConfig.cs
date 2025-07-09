ecf8de7199f8e3dc353ef65f391dbea08d8a1281
Rune T. Larsen <rune.tommeras.larsen@accenture.com>
2025-07-09 07:39:13 +0200

    Updated url

--- src/smartbank/smartbank.Server/Config/SmartBankConfig.cs
+++ src/smartbank/smartbank.Server/Config/SmartBankConfig.cs
@@ -2,11 +2,11 @@
 {
     public class SmartBankConfig
     {
-        public required string AT22PlatformEndpoint { get; set; } = "https://platform.tt02.altinn.no/";
+        public required string AT22PlatformEndpoint { get; set; } = "https://platform.at22.altinn.cloud/";
 
-        public required string AT23PlatformEndpoint { get; set; } = "https://platform.tt02.altinn.no/";
+        public required string AT23PlatformEndpoint { get; set; } = "https://platform.at23.altinn.cloud/";
 
-        public required string AT24PlatformEndpoint { get; set; } = "https://platform.tt02.altinn.no/";
+        public required string AT24PlatformEndpoint { get; set; } = "https://platform.at24.altinn.cloud/";
 
         public required string TT02PlatformEndpoint { get; set; } = "https://platform.tt02.altinn.no/";
 
